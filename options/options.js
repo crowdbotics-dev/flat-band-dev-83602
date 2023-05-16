0d9e4360d7d0d30551f4c9cf210439c262a4cdb1
Crowdbotics <engineering@crowdbotics.com>
2023-05-16 00:38:11 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "flat-band-dev-83602",
+  url: "https://flat-band-dev-83602.botics.co",
+  api: "https://flat-band-dev-83602.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
